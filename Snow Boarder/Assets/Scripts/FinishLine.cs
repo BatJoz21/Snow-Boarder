edd8e62e31be95c36967d0b63eb3caf7241782ba
Jozka Siregar <jozkasiregar@gmail.com>
2023-07-14 18:59:18 +0700

    main menu, crash and next level

--- Snow Boarder/Assets/Scripts/FinishLine.cs
+++ Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] private string levelName;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Debug.Log("Finish");
+            SceneManager.LoadScene(levelName);
         }        
     }
 }
