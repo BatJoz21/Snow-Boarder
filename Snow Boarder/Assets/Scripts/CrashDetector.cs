7eebe0df6a66d1b4e9a6aaa6e0976aea2dd4f1ab
Jozka Siregar <jozkasiregar@gmail.com>
2023-08-02 20:10:13 +0700

    update on particle effects

--- Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] private float reloadTime = 0.5f;
+    [SerializeField] private ParticleSystem crashEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Ground"))
         {
+            crashEffect.Play();
             Invoke("ReloadScene", reloadTime);
         }
 
--- Snow Boarder/Assets/Scripts/FinishLine.cs
+++ Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] private string levelName;
     [SerializeField] private float nextLevelTime = 0.5f;
+    [SerializeField] private ParticleSystem finishEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            finishEffect.Play();
             Invoke("NextLevel", nextLevelTime);
         }
-        
+
         void NextLevel()
         {
             SceneManager.LoadScene(levelName);
